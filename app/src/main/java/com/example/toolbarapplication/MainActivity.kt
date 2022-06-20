7e4945c5733a05260e647330ca933ea89bae3770
Vivek Modi <vivekmodi456@gmail.com>
2022-06-20 12:26:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/toolbarapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.toolbarapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.toolbarapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextActivity.setOnClickListener {
+            Intent(this, ToolbarActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/toolbarapplication/ToolbarActivity.kt
@@ -0,0 +1,68 @@
+package com.example.toolbarapplication
+
+import android.content.Context
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.example.toolbarapplication.databinding.ToolBarLayoutBinding
+
+private lateinit var binding: ToolBarLayoutBinding
+
+class ToolbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ToolBarLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        hideActionBar()
+        setupSearchView()
+    }
+
+    fun hideActionBar() {
+        supportActionBar?.hide()
+    }
+
+    fun setupSearchView() {
+        binding.consultationSearchView.apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?) = false
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText != null) {
+                    }
+                    return true
+                }
+            })
+            setOnQueryTextFocusChangeListener { view, hasFocus ->
+                binding.appBar.setExpanded(!hasFocus)
+                if (hasFocus) {
+                    binding.toolbar.apply {
+                        setSupportActionBar(this)
+                        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    }
+                }else{
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                isSelected = hasFocus
+            }
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val view: View? = currentFocus
+            if (view is SearchView.SearchAutoComplete) {
+                val outRect = Rect()
+                view.getGlobalVisibleRect(outRect);
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    view.clearFocus()
+                    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+}
