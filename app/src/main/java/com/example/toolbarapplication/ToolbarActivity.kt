45a06effc13c62811b555a650c65ddafc557f02e
Vivek Modi <vivekmodi456@gmail.com>
2022-06-21 11:14:41 +0100

    fixed toolbar collapse issue

--- app/src/main/java/com/example/toolbarapplication/ToolbarActivity.kt
+++ app/src/main/java/com/example/toolbarapplication/ToolbarActivity.kt
@@ -9,6 +9,7 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.example.toolbarapplication.databinding.ToolBarLayoutBinding
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 private lateinit var binding: ToolBarLayoutBinding
 
@@ -30,6 +31,7 @@ class ToolbarActivity : AppCompatActivity() {
     }
 
     fun setupSearchView() {
+        var originalMargin = 0
         binding.consultationSearchView.apply {
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?) = false
@@ -39,9 +41,18 @@ class ToolbarActivity : AppCompatActivity() {
                     return true
                 }
             })
+            val params =
+                binding.consultationSearchView.layoutParams as CollapsingToolbarLayout.LayoutParams
+            originalMargin = params.marginStart
             setOnQueryTextFocusChangeListener { view, hasFocus ->
                 binding.appBar.setExpanded(!hasFocus)
                 isSelected = hasFocus
+                if (hasFocus) {
+                    params.marginStart = originalMargin + 150 // arbitrary constant
+                } else {
+                    params.marginStart = originalMargin
+                }
+                view.layoutParams = params
             }
         }
     }
@@ -54,7 +65,8 @@ class ToolbarActivity : AppCompatActivity() {
                 view.getGlobalVisibleRect(outRect);
                 if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
                     view.clearFocus()
-                    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                     inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
             }
